22c90e985cada43dcd86d83d82889971d1975a61
Patrik Karlström <patrik@trixon.se>
2022-10-18 19:46:43 +0200

    Update license header

--- main/src/main/java/se/trixon/yaya/dice/OptionsPanel.java
+++ main/src/main/java/se/trixon/yaya/dice/OptionsPanel.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2018 Patrik Karlström.
+/* 
+ * Copyright 2022 Patrik Karlström <patrik@trixon.se>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
